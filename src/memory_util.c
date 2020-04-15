52807e2222abe34d97041e18393e15452651aa99
Piotr Prabucki <pp418377@students.mimuw.edu.pl>
2020-04-15 15:11:19 +0200

    using calloc instead of malloc got rid of all the valgrind errors

--- src/memory_util.c
+++ src/memory_util.c
@@ -3,12 +3,12 @@
 
 
 uint32_t** allocate_2d_array_uint32(uint64_t n, uint64_t m) {
-	uint32_t **arr = (uint32_t**)malloc(n * sizeof(uint32_t*));
+	uint32_t **arr = (uint32_t**)calloc(n, sizeof(uint32_t*));
 	if (!arr) {
 		return NULL;
 	}
 
-	uint32_t *arr_data = malloc(n * m * sizeof(uint32_t));
+	uint32_t *arr_data = calloc(n * m, sizeof(uint32_t));
 	if (!arr_data) {
 		return NULL;
 	}
@@ -22,12 +22,12 @@ uint32_t** allocate_2d_array_uint32(uint64_t n, uint64_t m) {
 
 
 uint64_t** allocate_2d_array_uint64(uint64_t n, uint64_t m) {
-	uint64_t **arr = (uint64_t**)malloc(n * sizeof(uint64_t*));
+	uint64_t **arr = (uint64_t**)calloc(n, sizeof(uint64_t*));
 	if (!arr) {
 		return NULL;
 	}
 
-	uint64_t *arr_data = malloc(n * m * sizeof(uint64_t));
+	uint64_t *arr_data = calloc(n * m, sizeof(uint64_t));
 	if (!arr_data) {
 		return NULL;
 	}
