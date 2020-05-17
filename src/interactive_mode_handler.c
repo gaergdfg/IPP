542970d6e4dfbb1b75ce64d0763cb37446bf8f84
Piotr Prabucki <pp418377@students.mimuw.edu.pl>
2020-05-17 12:41:27 +0200

    The Final Solution

--- src/interactive_mode_handler.c
+++ src/interactive_mode_handler.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
 #include "gamma.h"
@@ -35,29 +36,60 @@ int get_cell_size(uint32_t number) {
 /**
  * @brief Wyswietla plansze gry.
  * Wypisuje stan gry na standardowe wyjscie.
+ * Podswietla komorke ( @p cursor_pos_x, @p cursor_pos_y ).
  * 
- * @param[in] gamma     : wskaznik na strukture przechowujaca informacje o grze
- * @param[in] cell_size : rozmiar komorki planszy na wyjsciu
- * @param[in] player    : numer gracza, ktorego tura jest rozgrywana
+ * @param[in] gamma         : wskaznik na strukture przechowujaca informacje o
+ *                            grze
+ * @param[in] cell_size     : rozmiar komorki planszy na wyjsciu
+ * @param[in] player        : numer gracza, ktorego tura jest rozgrywana
+ * @param[in] cursor_pos_x  : numer kolumny komorki, na ktorej jest kursor
+ * @param[in] cursor_pos_y  : numer wiersza komorki, na ktorej jest kursor
  */
-void print_board(gamma_t *gamma, uint32_t cell_size, uint32_t player) {
+void print_board(
+	gamma_t *gamma, 
+	uint32_t cell_size,
+	uint32_t player,
+	long long cursor_pos_x,
+	long long cursor_pos_y
+) {
 	for (long long y = gamma->field_height - 1; y >= 0; y--) {
 		for (uint32_t x = 0; x < gamma->field_width; x++) {
-			if (x != gamma->field_width - 1) {
-				printf("%*d ", cell_size, *get_arr_32(gamma->field, x, y));
+			// highlighting the cursor position
+			if (y == gamma->field_height - cursor_pos_y && x == cursor_pos_x) {
+				printf("\033[46;1m");
 			}
-			else {
-				printf("%*d", cell_size, *get_arr_32(gamma->field, x, y));
+
+			// printing the cell's value or '.' if its not occupied
+			if (*get_arr_32(gamma->field, x, y) > 0) {
+				printf("%*u", cell_size, *get_arr_32(gamma->field, x, y));
+			} else {
+				char *dot = ".";
+				printf("%*s", cell_size, dot);
+			}
+
+			// ending the highlighting for the cursor
+			if (y == gamma->field_height - cursor_pos_y && x == cursor_pos_x) {
+				printf("\033[0m");
+			}
+
+			// adding a space if its not the last cell in the row
+			if (x != gamma->field_width - 1) {
+				printf(" ");
 			}
 		}
 		printf("\n");
 	}
-	printf("Tura gracza #%d\n", player);
-	printf("Zloty ruch ");
-	if (gamma->players[player - 1]->used_golden_move) {
-		printf("nie");
+	
+	printf(
+		"PLAYER %d %lu %lu", 
+		player,
+		gamma_busy_fields(gamma, player),
+		gamma_free_fields(gamma, player)
+	);
+	if (!gamma->players[player - 1]->used_golden_move) {
+		printf(" G");
 	}
-	printf("dostepny\n");
+	printf("\n");
 }
 
 
@@ -88,7 +120,7 @@ void reset_screen(
 ) {
 	clear_screen();
 
-	print_board(gamma, cell_size - 1, player);
+	print_board(gamma, cell_size - 1, player, row, column);
 
 	printf("\033[%d;%dH", column, (row + 1) * cell_size - 1);
 }
@@ -110,7 +142,10 @@ void move_cursor(
 	long long boundary_down,
 	long long boundary_up
 ) {
-	if (boundary_down <= *cursos_pos + diff && *cursos_pos + diff < boundary_up) {
+	if (
+		boundary_down <= *cursos_pos + diff && 
+		*cursos_pos + diff < boundary_up
+	) {
 		*cursos_pos += diff;
 	}
 }
@@ -161,31 +196,12 @@ long long get_next_player(
  * @param[in] boundary_y        : maksymalna poprawna pozycja kursora na osi Y
  * @param[in] max_player_number : liczba graczy w grze @p gamma
  */
-void run_interactive_mode(
+void handle_interactive_input(
 	gamma_t **gamma,
 	long long boundary_x,
 	long long boundary_y,
 	uint32_t max_player_number
 ) {
-	struct termios new_kbd_mode;
-	struct termios g_old_kbd_mode;
-
-	bool operation_failed = tcgetattr(0, &g_old_kbd_mode);
-	if (operation_failed) {
-		exit(1);
-	}
-
-	memcpy(&new_kbd_mode, &g_old_kbd_mode, sizeof (struct termios));
-
-	new_kbd_mode.c_lflag &= ~(ICANON | ECHO);
-	new_kbd_mode.c_cc[VTIME] = 0;
-	new_kbd_mode.c_cc[VMIN] = 1;
-
-	operation_failed = tcsetattr(0, TCSANOW, &new_kbd_mode);
-	if (operation_failed) {
-		exit(1);
-	}
-
 	int cell_size = get_cell_size(max_player_number) + 1;
 	long long cursor_pos_x = 0, cursor_pos_y = 1;
 	long long player = 1;
@@ -194,11 +210,11 @@ void run_interactive_mode(
 
 	char input;
 	int flag_escape = 0, flag_escape_followed = 0;
-	while ((input = getc(stdin))) {
+	while (!feof(stdin)) {
+		input = getc(stdin);
+
 		switch (input) {
 			case 4: // CTRL - D
-				clear_screen();
-				tcsetattr(0, TCSANOW, &g_old_kbd_mode);
 				return;
 			case 27: // ESCAPE
 				flag_escape = 1;
@@ -221,8 +237,6 @@ void run_interactive_mode(
 				else {
 					player = get_next_player(*gamma, player, max_player_number);
 					if (player == -1) {
-						clear_screen();
-						tcsetattr(0, TCSANOW, &g_old_kbd_mode);
 						return;
 					}
 				}
@@ -248,8 +262,6 @@ void run_interactive_mode(
 				) {
 					player = get_next_player(*gamma, player, max_player_number);
 					if (player == -1) {
-						clear_screen();
-						tcsetattr(0, TCSANOW, &g_old_kbd_mode);
 						return;
 					}
 				}
@@ -266,8 +278,6 @@ void run_interactive_mode(
 				) {
 					player = get_next_player(*gamma, player, max_player_number);
 					if (player == -1) {
-						clear_screen();
-						tcsetattr(0, TCSANOW, &g_old_kbd_mode);
 						return;
 					}
 				}
@@ -285,4 +295,54 @@ void run_interactive_mode(
 
 		reset_screen(*gamma, cursor_pos_x, cursor_pos_y, cell_size, player);
 	}
+}
+
+
+/**
+ * @brief Odpala tryb interaktywny gry @p gamma.
+ * Zmienia ustawienia wyswietlania terminala, by odpowiednio wypisywac stan gry.
+ * Przywraca te ustawienia przy wyjsciu z funkcji.
+ * 
+ * @param[in, out] gamma        : wskaznik na wskaznik na strukture
+ *                                przechowujaca stan gry
+ * @param[in] boundary_x        : maksymalna poprawna pozycja kursora na osi X
+ * @param[in] boundary_y        : maksymalna poprawna pozycja kursora na osi Y
+ * @param[in] max_player_number : liczba graczy w grze @p gamma
+ */
+void run_interactive_mode(
+	gamma_t **gamma,
+	long long boundary_x,
+	long long boundary_y,
+	uint32_t max_player_number
+) {
+	struct termios new_termios;
+	struct termios old_termios;
+
+	int operation_failed = tcgetattr(0, &old_termios);
+	if (operation_failed) {
+		exit(1);
+	}
+
+	memcpy(&new_termios, &old_termios, sizeof(struct termios));
+
+	new_termios.c_lflag &= ~(ICANON | ECHO);
+	new_termios.c_cc[VTIME] = 0;
+	new_termios.c_cc[VMIN] = 1;
+
+	operation_failed = tcsetattr(0, TCSANOW, &new_termios);
+	if (operation_failed) {
+		exit(1);
+	}
+
+	printf("\033[?25l");
+	handle_interactive_input(gamma, boundary_x, boundary_y, max_player_number);
+	printf("\033[?25h");
+
+	clear_screen();
+	operation_failed = tcsetattr(0, TCSANOW, &old_termios);
+	if (operation_failed) {
+		exit(1);
+	}
+
+	return;
 }
